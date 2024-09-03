0d816454b38f2e82d68e457a007d36616b861158
brianq-nisra <brian.quinn@nisra.gov.uk>
2024-09-03 13:36:17 +0100

    html exercise upload

--- Excel Exercise/Excel Exercise - Solution.R
+++ Excel Exercise/Excel Exercise - Solution.R
@@ -21,10 +21,8 @@ table_1_data <- readRDS(paste0(data_folder, "Trend/", current_year - 1, "/table_
 
 ## Function to calculate weighted percentage ####
 
-f_weighted_pct <- function (data, col, val) {
-  
-  
-  
+f_weighted_pct <- function(data, col, val) {
+  sum(data$W3[data[[col]] == val]) / sum(data$W3) * 100
 }
 
 ### Test our function ####
@@ -34,10 +32,10 @@ f_weighted_pct(data_final, "PCOS1", "Yes")
 
 ## Function to return unweighted n ####
 
-f_return_n <- function (data, col) {
-  
-  
-  
+f_return_n <- function(data, col) {
+  data %>%
+    filter(!is.na(data[[col]])) %>%
+    nrow()
 }
 
 ### Test our function ####
@@ -50,10 +48,16 @@ f_return_n(data_final, "TrustNISRA2")
 ## Use above functions along with mutate() command to add a new column ####
 
 table_1_data <- table_1_data %>%
-  mutate()
+  mutate(new = c(
+    f_weighted_pct(data_final, "PCOS1", "Yes"),
+    f_weighted_pct(data_final, "PCOS1", "No"),
+    f_weighted_pct(data_final, "PCOS1", "Don't know"),
+    f_return_n(data_final, "PCOS1")
+  ))
 
 ### Rename new column ####
 
+names(table_1_data) <- sub("new", current_year, names(table_1_data))
 
 
 # Excel stylings ####
@@ -85,31 +89,55 @@ wb <- createWorkbook()
 modifyBaseFont(wb, fontSize = 12, fontName = "Arial")
 
 addWorksheet(wb,
-             sheetName = "Heard_of_NISRA")
+  sheetName = "Heard_of_NISRA"
+)
 
 ## Write title to first row of sheet ####
 
+writeData(wb,
+  sheet = "Heard_of_NISRA",
+  x = paste0("Table 1: Awareness of NISRA, 2009-", current_year),
+  startCol = 1,
+  startRow = 1
+)
 
 ### Change styling of title ####
 
+addStyle(wb,
+  sheet = "Heard_of_NISRA",
+  style = title_style,
+  rows = 1,
+  cols = 1
+)
 
 ## Write data frame to sheet ####
 
+writeDataTable(wb,
+  sheet = "Heard_of_NISRA",
+  x = table_1_data,
+  startCol = 1,
+  startRow = 2,
+  headerStyle = header_style,
+  tableStyle = "none",
+  withFilter = FALSE
+)
 
 ### Change styling of table ####
 
 addStyle(wb,
-         sheet = "Heard_of_NISRA",
-         style = header_style_left,
-         rows = 2,
-         cols = 1)
+  sheet = "Heard_of_NISRA",
+  style = header_style_left,
+  rows = 2,
+  cols = 1
+)
 
 addStyle(wb,
-         sheet = "Heard_of_NISRA",
-         style = number_style,
-         rows = 3:6,
-         cols = 2:11,
-         gridExpand = TRUE)
+  sheet = "Heard_of_NISRA",
+  style = number_style,
+  rows = 3:6,
+  cols = 2:11,
+  gridExpand = TRUE
+)
 
 ### Change width of first column ####
 
@@ -117,4 +145,4 @@ setColWidths(wb, "Heard_of_NISRA", cols = 1, widths = 21)
 
 # Save Workbook out to local folder ####
 
-saveWorkbook(wb, "Excel-Coding.xlsx", overwrite = TRUE)
+saveWorkbook(wb, "Excel-Coding-Solution.xlsx", overwrite = TRUE)
--- Excel Exercise/Excel Exercise.R
+++ Excel Exercise/Excel Exercise.R
@@ -21,10 +21,8 @@ table_1_data <- readRDS(paste0(data_folder, "Trend/", current_year - 1, "/table_
 
 ## Function to calculate weighted percentage ####
 
-f_weighted_pct <- function (data, col, val) {
-  
+f_weighted_pct <- function(data, col, val) {
   sum(data$W3[data[[col]] == val]) / sum(data$W3) * 100
-  
 }
 
 ### Test our function ####
@@ -34,12 +32,14 @@ f_weighted_pct(data_final, "PCOS1", "Yes")
 
 ## Function to return unweighted n ####
 
-f_return_n <- function (data, col) {
-  
+f_return_n <- function(data, col) {
   data %>%
     filter(!is.na(data[[col]])) %>%
     nrow()
-  
+
+  # result <- data
+  # result <- filter(result, !is.na(result[[col]]))
+  # result <- nrow(result)
 }
 
 ### Test our function ####
@@ -52,15 +52,16 @@ f_return_n(data_final, "TrustNISRA2")
 ## Use above functions along with mutate() command to add a new column ####
 
 table_1_data <- table_1_data %>%
-  mutate(new = c(f_weighted_pct(data_final, "PCOS1", "Yes"),
-                 f_weighted_pct(data_final, "PCOS1", "No"),
-                 f_weighted_pct(data_final, "PCOS1", "Don't know"),
-                 f_return_n(data_final, "PCOS1")))
+  mutate(new = c(
+    f_weighted_pct(data_final, "PCOS1", "Yes"),
+    f_weighted_pct(data_final, "PCOS1", "No"),
+    f_weighted_pct(data_final, "PCOS1", "Don't know"),
+    f_return_n(data_final, "PCOS1")
+  ))
 
 ### Rename new column ####
 
 names(table_1_data) <- sub("new", current_year, names(table_1_data))
-
 
 # Excel stylings ####
 
@@ -91,50 +92,49 @@ wb <- createWorkbook()
 modifyBaseFont(wb, fontSize = 12, fontName = "Arial")
 
 addWorksheet(wb,
-             sheetName = "Heard_of_NISRA")
+  sheetName = "Heard_of_NISRA"
+)
 
 ## Write title to first row of sheet ####
-
-writeData(wb,
-          sheet = "Heard_of_NISRA",
-          x = paste0("Table 1: Awareness of NISRA, 2009-", current_year),
-          startCol = 1,
-          startRow = 1)
+writeData(wb, "Heard_of_NISRA",
+  x = paste0("Table 1: Awareness of NISRA, 2009-", current_year),
+  startCol = 1,
+  startRow = 1
+)
 
 ### Change styling of title ####
-
-addStyle(wb,
-         sheet = "Heard_of_NISRA",
-         style = title_style,
-         rows = 1,
-         cols = 1)
+addStyle(wb, "Heard_of_NISRA",
+  style = title_style,
+  rows = 1,
+  cols = 1
+)
 
 ## Write data frame to sheet ####
-
-writeDataTable(wb,
-               sheet = "Heard_of_NISRA",
-               x = table_1_data,
-               startCol = 1,
-               startRow = 2,
-               headerStyle = header_style,
-               tableStyle = "none",
-               withFilter = FALSE
-               )
+writeDataTable(wb, "Heard_of_NISRA",
+  x = table_1_data,
+  startCol = 1,
+  startRow = 2,
+  withFilter = FALSE,
+  tableStyle = "none",
+  headerStyle = header_style
+)
 
 ### Change styling of table ####
 
 addStyle(wb,
-         sheet = "Heard_of_NISRA",
-         style = header_style_left,
-         rows = 2,
-         cols = 1)
+  sheet = "Heard_of_NISRA",
+  style = header_style_left,
+  rows = 2,
+  cols = 1
+)
 
 addStyle(wb,
-         sheet = "Heard_of_NISRA",
-         style = number_style,
-         rows = 3:6,
-         cols = 2:11,
-         gridExpand = TRUE)
+  sheet = "Heard_of_NISRA",
+  style = number_style,
+  rows = 3:6,
+  cols = 2:11,
+  gridExpand = TRUE
+)
 
 ### Change width of first column ####
 
@@ -142,4 +142,4 @@ setColWidths(wb, "Heard_of_NISRA", cols = 1, widths = 21)
 
 # Save Workbook out to local folder ####
 
-saveWorkbook(wb, "Excel-Coding-Solution.xlsx", overwrite = TRUE)
+saveWorkbook(wb, "Excel-Coding.xlsx", overwrite = TRUE)
